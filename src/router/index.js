39e792e12f2e78bff9d06f471c28847b64ad546c
shannonliang <shannonliang312@gmail.com>
2017-04-17 15:32:32 +0800

    更新路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+import LoginRoute from '../components/login/route'
+import AdminUsersInfo from '../components/admin/user-info/route'
+import AdminToolsInfo from '../components/admin/office-tools/route'
+import AdminGiftsInfo from '../components/admin/gifts-info/route'
+import AdminAccountInfo from '../components/admin/admin-info/route'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+    { path: '/', redirect: '/login' },
+    ...LoginRoute,
+    ...AdminUsersInfo,
+    ...AdminToolsInfo,
+    ...AdminGiftsInfo,
+    ...AdminAccountInfo
   ]
 })
