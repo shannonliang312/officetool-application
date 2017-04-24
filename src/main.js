5127cb0ec2f43ee24dc7274ed29645201a631b45
shannonliang <shannonliang312@gmail.com>
2017-04-24 19:19:13 +0800

    增加路由拦截

--- src/main.js
+++ src/main.js
@@ -16,6 +16,32 @@ Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+/* 拦截路由 */
+router.beforeEach((to, from, next) => {
+  if(to.fullPath == "/login") {  //如果为登录页面，直接放行
+    next();
+  } else {
+    if(!store.state.login.isLogin) {      
+      next({path: '/login'});  //如果是非登录页面且登录状态为false，则跳转到登录页面
+    } else {
+      if(to.meta.requireAdmin) {
+        if(store.state.account.accountInfo.role == "admin") {
+          next();
+        } else {
+          next({path: '/login'}); //如果非admin用户访问了admin的路由，则跳转到登录页
+        }
+      } else {
+        if(store.state.account.accountInfo.role != "admin") {
+          next();
+        } else {
+          next({path: '/login'}); //如果admin用户访问了非admin的路由，则跳转到登录页
+        }
+      }      
+    }  
+  }
+  
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
